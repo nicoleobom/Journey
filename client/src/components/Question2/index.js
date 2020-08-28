7cf3b45d1a7cc13c73e824c662cf078188a29c36
Nicole O’Bomsawin <nicoleobomsawin@gmail.com>
2020-08-28 19:38:45 -0400

    redoing q2

--- client/src/components/Question2/index.js
+++ client/src/components/Question2/index.js
@@ -1,66 +1,14 @@
 import React from 'react';
-import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
-import './index.css';
+import { Dropdown } from 'react-bootstrap';
 
-
-class Question2 extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            newAddress: '',
-        }
+function Question2(props) {
+    if (props.currentQuestion !== 2) {
+        return null;
     }
-
-    handleChange = address => {
-        this.setState({ address });
-    }
-
-    handleSelect = address => {
-        geocodeByAddress(address)
-            .then(results => getLatLng(results[0]))
-            .then(latLng => console.log('Success', latLng))
-            .catch(error => console.log('Error', error));
-    };
-
-    render() {
-        return (
-            <div id="q2">
-                <h3>Where do you want to go?</h3>
-                <PlacesAutocomplete
-                    value={this.state.address}
-                    onChange={this.handleChange}
-                    onSelect={this.handleSelect}
-                >
-                    {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-                        <div>
-                            <input
-                                {...getInputProps({
-                                    placeholder: 'Search places...',
-                                    className: 'location-search-input',
-                                })}
-                                />
-                            <div className="autocomplete-dropdown-container">
-                                {loading && <div id="loading">Loading...</div>}
-                                {suggestions.map(suggestion => {
-                                    const className= suggestion.active ? 'suggestion-item--active' : 'suggestion-item';
-                                    const style = suggestion.active ? { backgroundColor: 'rgba(255,255,255, 0.6)', cursor: 'pointer' } : null;
-                                    return(
-                                        <div
-                                        {...getSuggestionItemProps(suggestion, {
-                                            className,
-                                            style,
-                                        })}>
-                                        
-                                        <span>{suggestion.description}</span>
-                                        </div>
-                                    );
-                                })}
-                            </div>
-                        </div>
-                    )}
-                </PlacesAutocomplete>
-
-            {/* <Dropdown>
+    return (
+        <div>
+            <h3>Where do you want to go?</h3>
+            <Dropdown>
                 <Dropdown.Toggle className="bgcolor">
                     Choose a country
                 </Dropdown.Toggle>
@@ -86,10 +34,9 @@ class Question2 extends React.Component {
                 <Dropdown.Menu>
                     <Dropdown.Item href="#/action-1">1</Dropdown.Item>
                 </Dropdown.Menu>
-            </Dropdown> */}
+            </Dropdown>
             </div>
         );
-    }
 
 }
 
