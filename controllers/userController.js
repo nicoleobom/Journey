a8efe92b418946cc57d3ef619d29da8e1769a160
Logan Hemphill <logan.hemphill86@gmail.com>
2020-08-28 11:49:03 -0400

    Adding routes, controllers and authentication

---
+++ controllers/userController.js
@@ -0,0 +1,39 @@
+const db = require("../models");
+
+module.exports = {
+	// find all users by score, sort by ascending
+	findAll: (req, res) => {
+		db.User
+			.find(req.query) // find all
+			.then(dbUser => res.json(dbUser))
+			.catch(err => res.status(422).json(err));
+	},
+
+	// find all by id
+	findById: (req, res) => {
+		db.User
+			.findById(req.params.id)
+			.then(dbUser => res.json(dbUser))
+			.catch(err => res.status(422).json(err));
+	},
+
+	// creating new users/posting to json
+	create: (req, res) => {
+		db.User
+			.create(req.body)
+			.then(data => res.redirect(307, "/api/user/login"))
+			.catch(err => res.status(422).json(err));
+	},
+
+	updateScore: (req, res) => {
+		db.User
+			.updateOne({ _id: req.body.id }, { $push: { scores: req.body.score } }, { upsert: true })
+			.then(data => res.json(data))
+			.catch(err => res.status(422).json(err));
+	},
+
+	login: async (req, res) => {
+		if (req.user) res.json(res.user);
+		res.send(null);
+	}
+}
--- routes/api/user.js
+++ routes/api/user.js
@@ -1,39 +1,11 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const userRoutes = express.Router();
-const passport = require('./config/passport');
-const session = require('express-session');
-const routes = require("./routes");
+const express = require("express");
+const router = express.Router();
+const userController = require("../../controllers/userController");
+const authenticate = require("../../config/authenticate");
+const passport = require("passport");
+const { User } = require("../../models");
 
-const PORT = process.env.PORT || 4000;
-
-let User = require('./models/user');
-
-app.use(cors());
-app.use(bodyParser.json());
-
-mongoose.connect('mongodb://localhost:27017/journey', { useNewUrlParser: true });
-const connection = mongoose.connection;
-
-connection.once('open', function() {
-    console.log('MongoDB database connection establish successfully.');
-})
-
-// session
-app.use(session({ secret: 'adventure', resave: true, saveUninitialized: true }));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use((req, res, next) => {
-	console.log('req.session', req.session);
-	return next();
-});
-
-// // user routes
+// user routes
 // userRoutes.route('/').get(function(req, res) {
 //     User.find(function(err, users) {
 //         if(err) {
@@ -43,6 +15,19 @@ app.use((req, res, next) => {
 //         }
 //     });
 // });
+
+router.get('/', userController.findAll);
+
+router.post("/login", passport.authenticate("local"), userController.login);
+
+router.get("/authenticate", authenticate);
+
+router.use(authenticate);
+
+router.get("/logout", (req, res) => {
+	req.logout();
+	res.redirect("/");
+});
 
 // userRoutes.route('/:id').get(function(req, res) {
 //     let id = req.params.id;
@@ -55,6 +40,8 @@ app.use((req, res, next) => {
 //     });
 // });
 
+router.get('/:id', userController.findById);
+
 // userRoutes.route('/add').post(function(req, res) {
 //     let user = new User(req.body);
 //     user.save()
@@ -65,6 +52,28 @@ app.use((req, res, next) => {
 //             res.status(400).send('adding new user failed.');
 //         });
 // });
+
+router.post('/add', userController.create);
+
+router.route('/update/:id').post(function(req, res) {
+    User.findById(req.params.id, function(err, user) {
+        if(!user)
+            res.status(404).send('data is not found.');
+        else
+            user.firstname = req.body.firstname;
+            user.lastname = req.body.lastname;
+            user.username = req.body.username;
+            user.password = req.body.username;
+            user.trips = req.body.trips;
+
+            user.save().then(todo => {
+                res.json('User updated!');
+            })
+            .catch(err => {
+                res.status(400).send('Update not possible.');
+            });
+    });
+});
 
 // userRoutes.route('/update/:id').post(function(req, res) {
 //     User.findById(req.params.id, function(err, user) {
@@ -86,12 +95,4 @@ app.use((req, res, next) => {
 //     });
 // });
 
-// trip routes - tbd
-
-app.use(routes);
-
-// app.use('/users', userRoutes);
-
-app.listen(PORT, function() {
-    console.log('Server is running on port: ' + PORT);
-});
+module.exports = router;
