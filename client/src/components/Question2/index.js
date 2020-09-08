d47d2a1b3ac77632c853444e3a9413d05ea2f79c
Nicole O’Bomsawin <nicoleobomsawin@gmail.com>
2020-09-08 19:28:15 -0400

    adding google api integration to q2

--- client/src/components/Question2/index.js
+++ client/src/components/Question2/index.js
@@ -2,6 +2,42 @@ import React from 'react';
 import './index.css';
 
 export default class Question2 extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            city: '',
+            query: '',
+        }
+    }
+ 
+    handleScriptLoad = () => {
+        const options = {
+            types: ['(cities)']
+        }
+ 
+        /*global google*/
+        this.autocomplete = new google.maps.places.Autocomplete(
+            document.getElementById('autocomplete'), options,
+        );
+ 
+        this.autocomplete.setFields(['address_components', 'formatted_address']);
+        this.autocomplete.addListener('places_changed', this.handlePlaceSelect);
+    }
+ 
+    handlePlaceSelect = () => {
+        debugger;
+        const addressObject = this.autocomplete.getPlaces();
+        const address = addressObject.address_components;
+ 
+        if(address) {
+            this.setState({
+                city: address[0].long_name,
+                query: addressObject.formatted_address,
+            })
+            this.props.handleChange('startpoint');
+        }
+    }
+
     back = (event) => {
         event.preventDefault();
         this.props.prevStep();
@@ -18,9 +54,10 @@ export default class Question2 extends React.Component {
             <div className="row" id="q1">
                 <form className="col-sm-12 header bg-q">
                     <h3>Where are you going?</h3>
-                    <input className="address" type="text" defaultValue={values.endpoint} placeholder="Enter city"></input><br />
-                    <input className="address" type="text" defaultValue={values.endpoint} placeholder="Enter state"></input><br />
-                    <input className="address" type="text" onChange={this.props.handleChange('endpoint')} defaultValue={values.endpoint} placeholder="Enter zip"></input>                   
+                    <input id="autocomplete"
+                        placeholder="Search cities"
+                        onChange={this.handleScriptLoad}
+                    />
                 </form>
                 <button className="next" onClick={this.nextQuestion}>Go</button>
                 <button className="back" onClick={this.back}>Back</button>
