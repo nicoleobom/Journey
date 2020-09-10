341316e7af504c748c85211cf6ee0551d7ec6aa5
Logan Hemphill <logan.hemphill86@gmail.com>
2020-09-10 07:11:12 -0400

    Q7 displaying properly

--- client/src/components/Question7/index.js
+++ client/src/components/Question7/index.js
@@ -19,28 +19,28 @@ export default class Question7 extends React.Component {
             <div className="row" id="q7">
                 <form className="col-sm-12 header bg-q">
                     <h3>What types of places do you want to stop at?</h3>
-                    <input id="national-parks" name="national-parks" type="checkbox" onChange={this.props.handleChange('stops')} />
+                    <input id="national-parks" name="national-parks" value="national-parks" type="checkbox" onClick={this.props.handleChange('stops')} />
                         <label htmlFor="national-parks">National Parks</label>
                     
-                    <input id="restaurants" name="restaurants" type="checkbox" onChange={this.props.handleChange('stops')} />
+                    <input id="restaurants" name="restaurants" value="restaurants" type="checkbox" onChange={this.props.handleChange('stops')} />
                         <label htmlFor="restaurants">Top-rated restaurants</label>
 
-                    <input id="dog-friendly" name="dog-friendly" type="checkbox" onChange={this.props.handleChange('stops')} />
+                    <input id="dog-friendly" name="dog-friendly" value="dog-friendly" type="checkbox" onChange={this.props.handleChange('stops')} />
                         <label htmlFor="dog-friendly">Dog-friendly areas</label>
 
-                    <input id="museums" name="museums" type="checkbox" onChange={this.props.handleChange('stops')} />
+                    <input id="museums" name="museums" value="museums" type="checkbox" onChange={this.props.handleChange('stops')} />
                         <label htmlFor="museums">Museums and art exhibits</label>
 
-                    <input id="coffee" name="coffee" type="checkbox" onChange={this.props.handleChange('stops')} />
+                    <input id="coffee" name="coffee" value="coffee" type="checkbox" onChange={this.props.handleChange('stops')} />
                         <label htmlFor="coffee">Best coffee</label>
 
-                    <input id="beaches-parks" name="beaches-parks" type="checkbox" onChange={this.props.handleChange('stops')} />
+                    <input id="beaches-parks" name="beaches-parks" value="beaches-parks" type="checkbox" onChange={this.props.handleChange('stops')} />
                         <label htmlFor="beaches-parks">Beaches and parks</label>
 
-                    <input id="food-trucks" name="food-trucks" type="checkbox" onChange={this.props.handleChange('stops')} />
+                    <input id="food-trucks" name="food-trucks" value="food-trucks" type="checkbox" onChange={this.props.handleChange('stops')} />
                         <label htmlFor="food-trucks">Food trucks</label>
 
-                    <input id="trails" name="trails" type="checkbox" onChange={this.props.handleChange('stops')} />
+                    <input id="trails" name="trails" value="trails" type="checkbox" onChange={this.props.handleChange('stops')} />
                         <label htmlFor="trails">Walking trails and bike paths</label>
 
                  </form>
