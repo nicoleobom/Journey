1c9ee976dfa24006158bc3709ab576bd9955548d
Logan Hemphill <logan.hemphill86@gmail.com>
2020-09-12 15:52:08 -0400

    pastTrips now mapping through user.trips

--- client/src/pages/PastTrips.js
+++ client/src/pages/PastTrips.js
@@ -1,30 +1,49 @@
 import React from 'react';
 import API from '../utils/API';
+import Moment from 'react-moment';
+// import PastTrip from '../components/PastTrip'
 
 export default class PastTrips extends React.Component {
     constructor(props) {
         super(props)
         this.state={
-            firstname: ""
+            firstname: "",
+            trips: []
         }
     }
 
     componentDidMount() {
-        this.userFirstName();
+        this.getUserData();
     }
 
-    userFirstName = async () => {
+    getUserData = async () => {
         const user = (await API.getUserData()).data;
         this.setState({
-            firstname: user.firstname
+            firstname: user.firstname,
+            trips: user.trips
         })
     }
 
     render() {
+        const userTrips = this.state.trips;
+        const filteredTrips = userTrips.filter(function (el) {
+            return el != null;
+          });
+        const trip = filteredTrips.map((trip) => 
+            <div>
+                <h4><Moment className="results" format="MMMM DD, YYYY">
+                {trip.endDate}
+                </Moment> Trip to {trip.endpoint}</h4>
+            </div>
+        );
+
         return(
             <div className="row">
                 <div className="col-sm-12 header">
                     <h3>{this.state.firstname}'s Trips</h3>
+                </div>
+                <div>
+                    {trip}
                 </div>
             </div>
         );
