f31b9a19370ba2c2f633e90ac60803a357237fc9
Nicole O’Bomsawin <nicoleobomsawin@gmail.com>
2020-09-26 16:49:58 -0400

    troubleshooting cannot get on deployed app

--- client/src/components/Question3/index.js
+++ client/src/components/Question3/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './index.css';
+import swal from 'sweetalert';
 
 export default class Question3 extends React.Component {
     back = (event) => {
@@ -12,24 +13,33 @@ export default class Question3 extends React.Component {
         this.props.nextStep();
     }
 
+    checkBudget() {
+        debugger;
+        const budget = this.props.values.budget;
+        if (budget < 100) {
+            swal('Budget must be at least $100.');
+        }
+    }
+
     render() {
         const { values } = this.props;
         return (
             <div className="row home-pg-2" id="q3">
-                <form className="col-sm-12 q-header bg-q">
+                <form className="col-sm-12 q-header bg-q" onSubmit={this.checkBudget}>
                     <h3>What's your budget?</h3>
-                    <span className="d-s">$</span><input id="budgetinput" type="number" min="100" step="100" onChange={this.props.handleChange('budget')} defaultValue={values.budget} placeholder="1000?"></input>
-                </form>
-                <div className="col-sm-12">
-                    <div className="row">
-                        <div className="col" style={{ paddingTop: "15px" }}>
-                            <button className="back" style={{ display: "block", margin: "auto" }} onClick={this.back}><i className="fas fa-angle-left fa-2x"></i></button>
-                        </div>
-                        <div className="col" style={{ paddingTop: "15px" }}>
-                            <button className="next" style={{ display: "block", margin: "auto" }} onClick={this.nextQuestion}><i className="fas fa-angle-right fa-2x"></i></button>
+                    <span className="d-s">$</span><input id="budgetinput" type="number" min="100" step="100" onChange={this.props.handleChange('budget')} defaultValue={values.budget}></input>
+                    <div className="col-sm-12">
+                        <div className="row">
+                            <div className="col" style={{ paddingTop: "15px" }}>
+                                <button className="back" style={{ display: "block", margin: "auto" }} onClick={this.back}><i className="fas fa-angle-left fa-2x"></i></button>
+                            </div>
+                            <div className="col" style={{ paddingTop: "15px" }}>
+                                <button type="submit" className="next" style={{ display: "block", margin: "auto" }} onClick={this.nextQuestion}><i className="fas fa-angle-right fa-2x"></i></button>
+                            </div>
                         </div>
                     </div>
-                </div>
+                    </form>
+
             </div>
         );
     }
